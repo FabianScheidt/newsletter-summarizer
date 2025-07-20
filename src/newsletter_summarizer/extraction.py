064046c4a7d4f11a4a402fffc3286e724acf720e
Fabian Scheidt <mail@fabian-scheidt.com>
2025-07-20 21:31:39 +0200

    feat: extend text extraction for future use

--- src/newsletter_summarizer/extraction.py
+++ src/newsletter_summarizer/extraction.py
@@ -1,24 +1,45 @@
 import json
-from typing import TypedDict
+from typing import TypedDict, List
 
 from bs4 import BeautifulSoup
 
 
+class ExtractedArticleTag(TypedDict):
+    name: str
+    uri: str
+    identifier: str
+
+
 class ExtractedArticle(TypedDict):
     id: str
+    href: str
+    published: str
+    updated: str
+    last_modified: str
     title: str
+    authors: List[str]
+    tags: List[ExtractedArticleTag]
     text: str
 
 
 def extract_article(html: str) -> ExtractedArticle:
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, "html.parser")
 
     next_data = soup.find("script", id="__NEXT_DATA__").text
     next_data_dict = json.loads(next_data)
     next_data_context = next_data_dict["props"]["pageProps"]["data"]["context"]
 
     id = next_data_context["id"]
+    href = next_data_context["href"]
+    published = next_data_context["published"]
+    updated = next_data_context["updated"]
+    last_modified = next_data_context["lastModified"]
     title = next_data_context["title"]
+    authors = [a["name"] for a in next_data_context["authors"]]
+    tags = [
+        ExtractedArticleTag(name=t["name"], uri=t["uri"], identifier=t["identifier"])
+        for t in next_data_context["tags"]
+    ]
 
     text = []
     for element in next_data_context["elements"]:
@@ -27,4 +48,14 @@ def extract_article(html: str) -> ExtractedArticle:
                 text.append(field["value"])
     text = "\n".join(text)
 
-    return ExtractedArticle(id=id, title=title, text=text)
+    return ExtractedArticle(
+        id=id,
+        href=href,
+        published=published,
+        updated=updated,
+        last_modified=last_modified,
+        title=title,
+        authors=authors,
+        tags=tags,
+        text=text,
+    )
